67e5d9281023cbd8bfeffac840f9bc16096962ec
chenwei-study <chenwei_study@163.com>
2019-11-26 14:26:39 +0800

    chenwei

--- handleVueProject/views.py
+++ handleVueProject/views.py
@@ -463,9 +463,9 @@ def upload_file(request):
         succeedcreate = pd.DataFrame()
         error = pd.DataFrame()
         df = pd.read_excel(wb)
-        if {'前缀', 'index', 'type', 'data'} <= set(list(df)):
-            select_df = df[['前缀', 'index', 'type', 'data']]
-            df2 = select_df.sort_values(by="前缀")
+        if {'prefix', 'index', 'type', 'data'} <= set(list(df)):
+            select_df = df[['prefix', 'index', 'type', 'data']]
+            df2 = select_df.sort_values(by="prefix")
             # 存在空白单元格处理
             havenul = df2[df2.isnull().T.any()]
             havenul['error'] = ('have  null')
@@ -482,7 +482,7 @@ def upload_file(request):
             same_df['error'] = ('have same vlue , system have creat once succeed you don not neet to creat again')
             error = error.append(same_df)
             nosame_df = no_index100.drop_duplicates()
-            group1 = nosame_df.groupby(nosame_df['前缀'])
+            group1 = nosame_df.groupby(nosame_df['prefix'])
             # 创建一个空的Dataframe
             errorprefix = pd.DataFrame()
             for a, b in group1:
@@ -537,8 +537,6 @@ def OneQuery(request):
     handle_record = reslove(perix, handle1.server.ip, handle1.server.port)
     handle1 = None
     handle1 = analyze_json(handle_record)
-    print handle1
-    handle1.context.pop()
     for i in range(len(handle1.context)):
         handle1.context[i].pop('timestamp')
     if handle_record is not None:
@@ -547,8 +545,9 @@ def OneQuery(request):
         data = list()
         i = 0
         for row in handle1.context:
-            i = i + 1
-            data.append(row)
+              if row.get('index') is not 100:
+                i = i + 1
+                data.append(row)
         if data == []:
             resp = {'status': 0, 'message': "标识不存在"}
             return HttpResponse(ujson.dumps(resp))
